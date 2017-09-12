a67cfee245393c895942edf19ca8db9b6206a5c5
G Roques <gbrwv8@umsl.edu>
2017-09-11 20:48:25 -0500

    Modify question 3 and answer questions 4 and 5

--- simplechain.c
+++ simplechain.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 int main (int argc, char *argv[]) {
 	pid_t childpid = 0;
-	int i, n;
-	if (argc != 2){
+	int i, j, n, k, m;
+	if (argc != 4){
 		/* check for valid number of command-line arguments */
-		fprintf(stderr, "Usage: %s processes\n", argv[0]);
+		fprintf(stderr, "Usage: %s processes number_of_iterations sleep_duration \n", argv[0]);
 		return 1;
 	}
+
 	n = atoi(argv[1]);
-	for (i = 1; i < n; i++)
+	k = atoi(argv[2]);
+	m = atoi(argv[3]);
+
+	for (i = 1; i < n; i++) {
 		if (childpid = fork())
 		  break;
-	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-          i, (long)getpid(), (long)getppid(), (long)childpid);
+	}
+
+	for (j = 0; j < k; j++) {
+		fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+    	i, (long)getpid(), (long)getppid(), (long)childpid);
+		sleep(m);
+	}
+
 	return 0;
 }
