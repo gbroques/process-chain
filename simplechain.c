78b4686125ace73fc027a625d2abe51ba9e1c2dd
G Roques <gbrwv8@umsl.edu>
2017-09-12 10:50:24 -0500

    Answer question 7

--- simplechain.c
+++ simplechain.c
@@ -22,8 +22,10 @@ int main (int argc, char *argv[]) {
 
 	for (j = 0; j < k; j++) {
 		wait();	
-		fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-    	i, (long)getpid(), (long)getppid(), (long)childpid); 
+		fprintf(stderr, "i:%d ", i);
+		fprintf(stderr, "process ID:%ld ", (long)getpid());
+		fprintf(stderr, "parent ID:%ld ", (long)getppid());
+		fprintf(stderr, "child ID:%ld\n", (long)childpid);
 		sleep(m);
 	}
 
